2fd393177697861019d40a09074b9cd74df43520
vitekvn01 <vitekvn01@yandex.ru>
2024-01-19 17:22:57 +0700

    Скрпит турели и урона

--- Ball Blast/Assets/Scenes/BallBlastSF/Scripts/CartInputConrol.cs
+++ Ball Blast/Assets/Scenes/BallBlastSF/Scripts/CartInputConrol.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class CartInputConrol : MonoBehaviour
 {
     [SerializeField] private Cart cart;
-
+    [SerializeField] private Turret turret;
+    [SerializeField] private float fireRate;
     private void Update()
     {
         cart.SetMovementTarget(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+
+        if (Input.GetMouseButton(0) == true)
+        {
+            Turret.Fire();
+        }
     }
 }
